b84deea01a36d1a63baefaa75e77e778a6e93145
5ia17sipeier <5ia17sipeier@gibwintra.ch>
2019-06-03 14:05:31 +0200

    Initial commit

---
+++ src/main/java/ch/bbw/gradesaver/GradeSaverApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.gradesaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSaverApplication.class, args);
+	}
+
+}
